f4dca96f882ac76a42e273284637683ea9e9bba9
Hemn Movafeghi <hemn791@gmail.com>
2022-01-20 18:39:15 +0300

    add accounts

---
+++ database/migrations/2022_01_20_151314_create_accounts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("user_id")->references("id")->on("users")->cascadeOnDelete();
+            $table->json("credentials");
+            $table->string("exchange")->default("binance");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
